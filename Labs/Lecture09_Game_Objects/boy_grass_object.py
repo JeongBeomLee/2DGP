da4222281d2db6c889718af3d05f39f8ee27810b
Teterschaft <gofla2242@tukorea.ac.kr>
2022-10-10 13:24:46 +0900

    Update boy_grass_object.py

--- Labs/Lecture09_Game_Objects/boy_grass_object.py
+++ Labs/Lecture09_Game_Objects/boy_grass_object.py
@@ -1,6 +1,26 @@
 from pico2d import *
+import random
 
 # Game object class here
+class Grass:
+    def __init__(self):
+        self.image = load_image('grass.png')
+
+    def draw(self):
+        self.image.draw(400, 30)
+
+class Boy:
+    def __init__(self):
+        self.x, self.y = random.randint(100, 700), 90
+        self.frame = random.randint(0, 7)
+        self.image = load_image('run_animation.png')
+
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+        self.x += 5
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
 
 def handle_events():
     global running
@@ -12,7 +32,31 @@ def handle_events():
             running = False
 
 # initialization code
+open_canvas()
+
+grass = Grass()
+team = [Boy() for i in range(11)]
+
+running = True
 
 # game main loop code
+while(running):
+    handle_events()
+
+    # boy.update()
+    for boy in team:
+        boy.update()
+
+    clear_canvas()
+    grass.draw()
+    # boy.draw()
+    for boy in team:
+        boy.draw()
+    update_canvas()
+
+    delay(0.05)
 
 # finalization code
+del boy
+del grass
+close_canvas()
